4a63e9732343c4fbd149659b594088291d5ab8bd
maeve <maevestites@gmail.com>
2020-06-12 12:07:11 -0400

    Write group function and load vars

--- censusData/datasetBuild.R
+++ censusData/datasetBuild.R
@@ -5,32 +5,6 @@
 # * validate results
 # * loop through and than concat at end?
 
-# SCRAPPED:
-# incM <- c(incTot = "B20001_001", inc0m = "B20001_003", inc1m = "B20001_004",
-#           inc2m = "B20001_005", inc3m = "B20001_006", inc4m = "B20001_007",
-#           inc5m = "B20001_008", inc6m = "B20001_009", inc7m = "B20001_010",
-#           inc8m = "B20001_011", inc9m = "B20001_012", inc10m = "B20001_013",
-#           inc11m = "B20001_014", inc12m = "B20001_015", inc13m = "B20001_016",
-#           inc14m = "B20001_017", inc15m = "B20001_018", inc16m = "B20001_019",
-#           inc17m = "B20001_020", inc18m = "B20001_021", inc19m = "B20001_022")
-# incF <- c(inc0f = "B20001_024", inc1f = "B20001_025",
-#           inc2f = "B20001_026", inc3f = "B20001_027", inc4f = "B20001_028",
-#           inc5f = "B20001_029", inc6f = "B20001_030", inc7f = "B20001_031",
-#           inc8f = "B20001_032", inc9f = "B20001_033", inc10f = "B20001_034",
-#           inc11f = "B20001_035", inc12f = "B20001_036", inc13f = "B20001_037",
-#           inc14f = "B20001_038", inc15f = "B20001_039", inc16f = "B20001_040",
-#           inc17f = "B20001_041", inc18f = "B20001_042", inc19f = "B20001_043")
-
-# educ <- c(educTot = "B15003_001", noSch = "B15003_002", nursery = "B15003_003", 
-#           kinder = "B15003_004", first = "B15003_005", sec = "B15003_006", 
-#           third = "B15003_007", fourth = "B15003_008", fifth = "B15003_009", 
-#           sixth = "B15003_010", seventh = "B15003_011", eigth = "B15003_012", 
-#           ninth = "B15003_013", tenth = "B15003_014", eleventh = "B15003_015",
-#           twelfth = "B15003_016", high = "B15003_017", GED = "B15003_018",
-#           sCollege1 = "B15003_019", sCollege2 = "B15003_020", assoc = "B15003_021",
-#           bachelor = "B15003_022", master = "B15003_023", profSchool = "B15003_024",
-#           phd = "B15003_025") # verified
-
 
 library(tidyverse)
 library(tidycensus)
@@ -53,18 +27,25 @@ inc <- c(medFamInc = "B19113_001", perCapitaInc = "B19301_001",
          inc5 = "B19001_007", inc6 = "B19001_008", inc7 = "B19001_009", 
          inc8 = "B19001_010", inc9 = "B19001_011", inc10 = "B19001_012", 
          inc11 = "B19001_013", inc12 = "B19001_014", inc13 = "B19001_015", 
-         inc14 = "B19001_016", inc15 = "B19001_017")
+         inc14 = "B19001_016", inc15 = "B19001_017") # verified
+pov <- c(sampPov = "B17020_001", 
+         Poverty = "B17020_002", abovePoverty ="B17020_010") # verified
 educ <- c(sampEduc = "B06009_001", uHigh = "B06009_002", 
           High = "B06009_003", sCollOrAssoc = "B06009_004", 
           Bachelor = "B06009_005", Graduate = "B06009_006") # verified
-
-cit <- c(NotCitizen = "B05001_006") # verified
-dis <- c(mDisability = "B18101_002", fDisability = "B18101_021") # verified
-
-# B06007_0## vars used as B16001_0## vars were NA
-lang <- c(langTot = "B06007_001", engOnly = "B06007_002", spanish = "B06007_003",
-          spanishStrE = "B06007_004", spanishWeakE = "B06007_005")
-vars = c(pop, educ)
+net <- c(sampInt = "B28011_001", intSubs = "B28011_002",
+         intAccess = "B28011_007", noIntAccess = "B28011_008") # verified
+cit <- c(usBornCit = "B05001_002", pBornCit = "B05001_003", 
+         aBornCit = "B05001_004", natCit = "B05001_005", 
+         NotCitizen = "B05001_006") # verified
+dis <- c(sampDis = "B18135_001", 
+         Dis19 = "B18135_003", nDis19 = "B18135_008",
+         Dis64 = "B18135_014", nDis64 = "B18135_019", 
+         Dis65 = "B18135_025", nDis65 = "B18135_030") # verified
+lang <- c(sampLang = "B06007_001", engOnly = "B06007_002", 
+          spanish = "B06007_003", spanishStrE = "B06007_004", 
+          spanishWeakE = "B06007_005") # verified
+vars = c(pop, sex, race, cit, lang, inc, pov, net, dis)
 
 # get data
 riPop <- get_acs(geography = "tract", 
@@ -77,9 +58,33 @@ riPop <- get_acs(geography = "tract",
 # trim, reshape to tidy, constrain to providence reshape to be tidy
 riPop = riPop %>% select(c('GEOID','NAME','variable','estimate')) %>%
   spread(key='variable',value='estimate')
-
+print(riPop)
 # constrain to providence
 riPop = riPop[riPop$GEOID < 44007010000,]
 
 # sort columns
 riPop = riPop[c('GEOID', 'NAME', names(vars))]
+
+# sel to which one to delete
+group <- function(df, toGroup, colName){
+  for (i in 2:length(toGroup)){
+    df[toGroup[1]] <- df[toGroup[1]] + df[toGroup[i]] # add columns
+    df[toGroup[i]] <- NULL # remove col
+  }
+  names(df)[names(df)==toGroup[1]] <- colName # rename col
+  return(df)
+}
+riPop = riPop %>% group(c('intSubs', 'intAccess'), 'IntAccess') %>%
+  group(c('Dis19', 'Dis64', 'Dis65'), 'Disability') %>%
+  group(c('nDis19', 'nDis64', 'nDis65'), 'NoDisability') %>%
+  group(c('usBornCit', 'pBornCit', 'aBornCit', 'natCit'), 'Citizen') %>%
+  group(c('Native', 'Asian', 'HIPI', 'oAlone', 'oTwo'), 'OtherRace') %>%
+  # simplify income brackets
+  group(c('inc0', 'inc1'), 'inc0') %>% # < 15k
+  group(c('inc2', 'inc3'), 'inc1') %>% # 15-25k
+  group(c('inc4', 'inc5'), 'inc2') %>% # 25-35k
+  group(c('inc6', 'inc7'), 'inc3') %>% # 35-45k
+  group(c('inc8', 'inc9'), 'inc4') %>% # 45-60k
+  group(c('inc10', 'inc11'), 'inc5') %>% # 60-100k
+  group(c('inc12', 'inc13'), 'inc6') %>% # 100-150k
+  group(c('inc14', 'inc15'), 'inc7') # > 150k
