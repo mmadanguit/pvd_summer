08c750b0892d4ba71254987b4ef95e9dbab05952
nolanflynn <nolanflynn0@gmail.com>
2020-06-15 12:59:15 -0400

    Added comments and clarified some stuff

--- app.R
+++ app.R
@@ -20,7 +20,7 @@ ui <- fluidPage(
       
       # Input: Choose variable ----
       radioButtons("varToPlot", "Variable:",
-                   c('Male','White', 'Black','Hispanic',"Age"))
+                   c('Male','White', 'Black','Hispanic',"Age")) #Defines which checkboxes to use
       
     ),
     
@@ -37,20 +37,22 @@ ui <- fluidPage(
 # Define server logic to summarize and view selected dataset ----
 server <- function(input, output) {
   # Return the requested dataset ----
+  # print(input)
   output$distPlot <- renderLeaflet({
-    pal <- colorQuantile(palette = "viridis", domain = ri_pop$input$varToPlot, n = 10)
+    # pal <- colorNumeric(palette = "viridis", domain = ri_pop$input$varToPlot, n = 10) #This line scales the colors from the minimum to the maximum value for the selected variable
+    pal <- colorNumeric(palette = "viridis", domain = 0:1, n = 10) #This line scales the colors from 0 to 1 for all variables, allowing us to see different info. 
     ri_pop %>% 
-      st_transform(crs = "+init=epsg:4326") %>%
-      leaflet() %>%
-      addProviderTiles(provider = "CartoDB.Positron") %>%
-      addPolygons(stroke = FALSE,
+      st_transform(crs = "+init=epsg:4326") %>% #Defines the geography info format
+      leaflet() %>% #Creates leaflet pane
+      addProviderTiles(provider = "CartoDB.Positron") %>% #No clue what this does tbh
+      addPolygons(stroke = FALSE, #Creates the polygons to overlay on the map, with parameters
                   smoothFactor = 0,
                   fillOpacity = 0.7,
                   color = ~ pal(get(input$varToPlot))) %>%
-      addLegend("bottomright", 
+      addLegend("bottomright", #Adds the legend
                 pal = pal, 
                 values = ~ get(input$varToPlot),
-                title = "Population percentiles",
+                title = "Population fraction",
                 opacity = 1)
   })
   
--- census_data_example.R
+++ census_data_example.R
@@ -5,6 +5,8 @@ library(leaflet)
 library(stringr)
 
 
+#Old stuff from when we were loading all from get_acs, now uing Maeve's dataset
+
 # race_vars <- c(
 #   White = "B03002_003", 
 #   Black = "B03002_004", 
@@ -26,19 +28,23 @@ library(stringr)
 # # reshape and select within city boundaries
 # ri_pop = ri_pop %>% select(c('GEOID','NAME','variable','estimate')) %>%
 #   spread(key='variable',value='estimate')
-ri_pop_data <- read.csv("censusData/riData.csv")
-ri_pop_geo <- get_acs(geography = "tract", 
+
+
+ri_pop_data <- read.csv("censusData/riData.csv") #Load Maeve's dataset
+ri_pop_geo <- get_acs(geography = "tract", #Load a single variable from get_acs because Maeve's dataset does not contain the geodata
                       state = "RI", 
                       county = 'Providence County',
                       variables = "B03002_003",
                       geometry = TRUE,
                       cache_table = TRUE) 
-ri_pop_geo = ri_pop_geo[ri_pop_geo$GEOID < 44007010000,] %>% select(c('GEOID','geometry'))
-ri_pop <- merge(ri_pop_geo, ri_pop_data)
-# ri_pop = ri_pop[ri_pop$GEOID < 44007010000]
+ri_pop_geo = ri_pop_geo[ri_pop_geo$GEOID < 44007010000,] %>% #Only get the census tracts within Providence City
+  select(c('GEOID','geometry')) #Only keep the GEOID, which corresponds to the census tract, and the geometry
+ri_pop <- merge(ri_pop_geo, ri_pop_data) #Merge the sf dataframe with just the geometry with the dataframe loaded from Maeve's CSV
 
 #write.csv(ri_pop,file='ri_data.csv')
 
+
+# I believe this is all old stuff to do a Leaflet display, now in app.R
 # pal <- colorQuantile(palette = "viridis", domain = ri_pop$White, n = 10)
 # 
 # # plot
