54290cd748adacfb2d50b931f48cdc4cd69edfa2
nolanflynn <nolanflynn0@gmail.com>
2020-06-19 11:14:43 -0400

    Added all variables from Maeve's census data csv. Added horizontal lines in between each map. Created labels for each census tract with the tract name and the value of the selected variable for that census tract.

--- app.R
+++ app.R
@@ -20,8 +20,33 @@ ui <- fluidPage(
       
       # Input: Choose variable ----
       checkboxGroupInput("varToPlot", "Variable:",
-                   c('Male','White', 'Black','Hispanic',"Age")) #Defines which checkboxes to use
-      
+                   c('Pop',
+                     'Male',
+                     'Female',
+                     'White', 
+                     'Black',
+                     'Other', 
+                     'Hispanic', 
+                     'Citizen',
+                     'NotCitizen',
+                     'engOnly',
+                     'spanish',
+                     'spanishStrE',
+                     'spanishWeakE',
+                     'medFamInc',
+                     'perCapitaInc',
+                     'Poverty',
+                     'abovePoverty',
+                     'InternetAccess',
+                     'noInternetAccess',
+                     'Disability',
+                     'NoDisability',
+                     'auto',
+                     'public',
+                     'walk',
+                     'other',
+                     'college'
+                     )) #Defines which checkboxes to use
     ),
     
     # Main panel for displaying outputs ----
@@ -35,8 +60,6 @@ ui <- fluidPage(
 
 # Define server logic to summarize and view selected dataset ----
 server <- function(input, output) {
-  
-  
   currentIds <- c()
   # Return the requested dataset ----
   observeEvent(input$varToPlot, { #Trigger all this mapping when the checkboxes change
@@ -46,28 +69,46 @@ server <- function(input, output) {
     currentIds <<- c()
     for(item in input$varToPlot){ #For every checked checkbox
       constraintId <- paste0('Constraint_', item) #Make this constraint ID
-      currentIds <<- c(constraintId, currentIds) #And add this ID to this list
-      # pal <- colorNumeric(palette = "viridis", domain = ri_pop$input$varToPlot[index], n = 10) #This line scales the colors from the minimum to the maximum value for the selected variable
-      pal <- colorNumeric(palette = "viridis", domain = 0:1, n = 10) #This line scales the colors from 0 to 1 for all variables, allowing us to see different info.
-      
+      horizontalId <- paste0('Constraint2_', item) #Make this constraint ID
+      currentIds <<- c(constraintId, horizontalId, currentIds) #And add this ID to this list
+      if(item == 'Pop' | item == 'medFamInc' | item == 'perCapitaInc'){ #If the item is any of these, scale according to the domain becuz these are the variables that are not 0 to 1 range
+        pal <- colorNumeric(palette = "plasma", domain = ri_pop$item, n = 10) #This line scales the colors from the minimum to the maximum value for the selected variable
+      } else { #For all the 0 to 1 range percentage things, do one of these
+        # pal <- colorNumeric(palette = "plasma", domain = ri_pop$item, n = 10) #This line scales the colors from the minimum to the maximum value for the selected variable
+        pal <- colorNumeric(palette = "plasma", domain = 0:1, n = 10) #This line scales the colors from 0 to 1 for all variables, allowing us to see different info.
+      }
+      labels <- sprintf( #Make a list of labels with HTML styling for each census tract
+        "<strong>%s</strong><br/>variable = %g",
+        str_extract(ri_pop$NAME, "^([^,]*)"), ri_pop[[item]] #Fill in the tract name, formatted to just the census tract number, and the value of this column
+      ) %>% lapply(htmltools::HTML)
       insertUI( #Add a UI element
         selector = '#placeholderConstraint', #After the placeholderConstraint div
         ui = tags$div(id = constraintId, #Give this UI div an ID of the previously defined ID name
-              ri_pop %>% #Put this big ol map thing inside this div
-                st_transform(crs = "+init=epsg:4326") %>% #Defines the geography info format
-                leaflet() %>% #Creates leaflet pane
-                addProviderTiles(provider = "CartoDB.Positron") %>% #No clue what this does tbh
-                addPolygons(stroke = FALSE, #Creates the polygons to overlay on the map, with parameters
-                            smoothFactor = 0,
-                            fillOpacity = 0.7,
-                            color = ~ pal(get(item))) %>%
-                addLegend("bottomright", #Adds the legend
-                          pal = pal,
-                          values = ~ get(item),
-                          title = "Population fraction",
-                          opacity = 1)
-              
+          ri_pop %>% #Put this big ol map thing inside this div
+            st_transform(crs = "+init=epsg:4326") %>% #Defines the geography info format
+            leaflet() %>% #Creates leaflet pane
+            addProviderTiles(provider = "CartoDB.Positron") %>% #No clue what this does tbh
+            addPolygons(
+              stroke = FALSE, #Creates the polygons to overlay on the map, with parameters
+              smoothFactor = 0,
+              fillOpacity = 0.7,
+              color = ~ pal(get(item)),
+              label = labels, #Add the label
+              labelOptions = labelOptions( #Add label styling
+                style = list("font-weight" = "normal", padding = "3px 8px"),
+                textsize = "15px",
+                direction = "auto")
+            ) %>%
+            addLegend("bottomright", #Adds the legend
+              pal = pal,
+              values = ~ get(item),
+              title = "Population fraction",
+              opacity = 1)
         )
+      )
+      insertUI( #Add a horizontal line
+        selector = '#placeholderConstraint', #After the placeholderConstraint div
+        ui = tags$hr(id=horizontalId)
       )
     }
   })
