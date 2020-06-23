52e33d908290dadae56eb877e2e0e88d4f81ca43
mstites <maeve@mstites.com>
2020-06-23 14:52:24 -0400

    Add geometry data and type support for mapping

--- demand/demandModel.R
+++ demand/demandModel.R
@@ -12,18 +12,29 @@ dt <- as.data.frame(str_split_fixed(trips$event_time, " ", 2)) # separate date a
 trips <- trips %>% select(c('TRACT','n')) %>% cbind(DATE = dt$V1, TIME = dt$V2) 
 trips$day <- weekdays(as.Date(trips$DATE))
 
-# DAILY DEMAND
-demand <- trips %>% group_by(TRACT) %>% summarize(average = mean(n), standard_deviation=sd(n))
-# demand$GEOMETRY <- 
-
 # LOAD GEODATA AND SIMPLIFY TRACT #s
 geoData <- readRDS("censusData/riDataGeo.Rds")
 geoData$TRACT = as.double(geoData$NAME %>% str_remove_all("\\p{Letter}") %>% 
                             str_remove_all("\\N{Comma}") %>%
                             str_remove_all("\\h")) # white space
+# geoData <- geoData[order(geoData$TRACT),] #reorder
+geoData <- geoData %>% arrange(geoData$TRACT)
+
+# DAILY DEMAND
+demand <- trips %>% group_by(TRACT) %>% summarize(average = mean(n), standard_deviation=sd(n))
+demand$geometry <- geoData %>% filter(as.logical(match(geoData$TRACT, demand$TRACT)))$geometry
+demand <- st_as_sf(demand)
+# demand$geometry <- as_Spatial(demand$geometry)
+# demand$GEOMETRY <- 
+# want to create NA rows for all tracts where data does not exist
+# everywhere there is an NA, make a row in the demand dataframe
+# verify matching
+# then can add the column of geography data with c apply
+# rbind?
 
 # time <- subset(trips, DATE > "2020-01-03" & DATE < "2020-01-10") # filter by time period
 time <- trips %>% subset(DATE == "2020-01-07")
-mapview(geoData) # map data needs GEOID, name, and geometry
+# mapview(Spatial) # map data needs GEOID, name, and geometry
+mapview(demand)
 
 
