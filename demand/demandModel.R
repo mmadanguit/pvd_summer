49d6419f1b04cd60525246f9c1916a48cc4c913a
mstites <maeve@mstites.com>
2020-06-23 14:57:51 -0400

    Add support for mapping

--- demand/demandModel.R
+++ demand/demandModel.R
@@ -21,20 +21,15 @@ geoData$TRACT = as.double(geoData$NAME %>% str_remove_all("\\p{Letter}") %>%
 geoData <- geoData %>% arrange(geoData$TRACT)
 
 # DAILY DEMAND
-demand <- trips %>% group_by(TRACT) %>% summarize(average = mean(n), standard_deviation=sd(n))
-demand$geometry <- geoData %>% filter(as.logical(match(geoData$TRACT, demand$TRACT)))$geometry
-demand <- st_as_sf(demand)
-# demand$geometry <- as_Spatial(demand$geometry)
-# demand$GEOMETRY <- 
-# want to create NA rows for all tracts where data does not exist
-# everywhere there is an NA, make a row in the demand dataframe
-# verify matching
-# then can add the column of geography data with c apply
-# rbind?
-
 # time <- subset(trips, DATE > "2020-01-03" & DATE < "2020-01-10") # filter by time period
+demand <- trips %>% group_by(TRACT) %>% summarize(average = mean(n), standard_deviation=sd(n))
+demandGeo <- (geoData %>% filter(as.logical(match(geoData$TRACT, demand$TRACT))))
+demand$GEOMETRY <- demandGeo$geometry
+demand$NAME <- demandGeo$NAME
+demand <- st_as_sf(demand)
+
 time <- trips %>% subset(DATE == "2020-01-07")
 # mapview(Spatial) # map data needs GEOID, name, and geometry
-mapview(demand)
+mapview(demand, zcol = "average")
 
 
