74c6a7c2c1606777117da41d6b6d29a37ed19c81
nolanflynn <nflynn19@riverdale.edu>
2020-06-26 12:07:18 -0400

    Switched from pulling geometry data from tidycensus to using the geometry data RDS that Maeve created

--- census_data_example.R
+++ census_data_example.R
@@ -5,13 +5,6 @@ library(leaflet)
 library(stringr)
 
 
-ri_pop_data <- read.csv("censusData/riData.csv") #Load Maeve's dataset
-ri_pop_geo <- get_acs(geography = "tract", #Load a single variable from get_acs because Maeve's dataset does not contain the geodata
-                      state = "RI", 
-                      county = 'Providence County',
-                      variables = "B03002_003",
-                      geometry = TRUE,
-                      cache_table = TRUE) 
-ri_pop_geo = ri_pop_geo[ri_pop_geo$GEOID < 44007010000,] %>% #Only get the census tracts within Providence City
-  select(c('GEOID','geometry')) #Only keep the GEOID, which corresponds to the census tract, and the geometry
+ri_pop_data <- read.csv("censusData/riData.csv") #Load Maeve's dataset of census data
+ri_pop_geo <- readRDS("censusData/riDataGeo.Rds") #Load Maeve's dataset of geometry data
 ri_pop <- merge(ri_pop_geo, ri_pop_data) #Merge the sf dataframe with just the geometry with the dataframe loaded from Maeve's CSV
