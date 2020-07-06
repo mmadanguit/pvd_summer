70945802e2d1a0c8d97b784b2c522874b3ce5e9a
mstites <maeve@mstites.com>
2020-07-06 15:08:29 -0400

    Organize code into functions

--- demand/availabilityDemandModel.R
+++ demand/availabilityDemandModel.R
@@ -4,19 +4,30 @@ library(mapview)
 source("intervals.R")
 # setwd("~/Documents/github/pvd_summer/demand")
 
-# import data
-locData <- locations_and_tract_18_09_01_to_19_11_01 %>% # load
-  select(-c(provider, vehicle_status, vehicle_status_reason, 
-            device_type, areas, lat, lng)) %>% # remove unwanted columns
-  arrange(TRACT) %>% group_by(TRACT) %>% arrange(start_time, .by_group = TRUE) %>%
-  filter(TRACT <= 37)# restrict to our TRACTS
+clean <- function(df){
+  "Selects only the location data needed"
+  df <- df %>% select(-c(provider, vehicle_status, vehicle_status_reason, 
+              device_type, areas, lat, lng)) %>% # remove unwanted columns
+    arrange(TRACT) %>% group_by(TRACT) %>% 
+    arrange(start_time, .by_group = TRUE) %>%
+    filter(TRACT <= 37)
+  return(df)
+}
 
-# split date and time
-start <- str_split_fixed(locData$start_time, " ", 2)
-end <- str_split_fixed(locData$end_time, " ", 2)
-locData <- locData %>% select(-c(start_time, end_time)) %>% 
-  add_column(startDate = start[,1], endDate = end[,1], 
-             startTime = start[,2], endTime = end[,2])
+splitTimeCol <- function(df){
+  "Splits the data columns into seperate day and time"
+  start <- str_split_fixed(df$start_time, " ", 2)
+  end <- str_split_fixed(df$end_time, " ", 2)
+  df <- df %>% select(-c(start_time, end_time)) %>% 
+    add_column(startDate = start[,1], endDate = end[,1], 
+               startTime = start[,2], endTime = end[,2])
+  return(df)
+}
+
 period <- as.character(
   seq(as.Date("2019-10-22"), as.Date("2019-11-8"), by = "day"))
-intervalData <- getIntervalData(locData, period)
+locData <- locations_and_tract_and_rounded_latlong_from_oct18_oct19
+locData <- clean(locData) %>% splitTimeCol %>%
+  filter((startDate %in% period) & (endDate %in% period)) # restrict to period
+
+# intervalData <- getIntervalData(locData, period)
