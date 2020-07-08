d50a51a07bd93fa5c83ea86a237138bbe438ab27
mstites <maeve@mstites.com>
2020-07-08 16:24:27 -0400

    Filter to counted time

--- demand/availability/dataPrep/pickups.R
+++ demand/availability/dataPrep/pickups.R
@@ -2,10 +2,20 @@ library(tidyverse)
 setwd("~/Documents/github/pvd_summer/demand/availability/dataPrep") # change to your WD
 source('mapToTract.R')
 
+splitTimeCol <- function(df){
+  "Splits the data column into seperate day and time"
+  split <- str_split_fixed(df$event_time, " ", 2)
+  df <- df %>% select(-c(event_time)) %>% 
+    add_column(DATE = split[,1], TIME = split[,2])
+  return(df)
+}
+
 preClean <- function(df){
   "Select only relevant data"
   df <- df %>% filter(event_type_reason == "user_pick_up") %>%
+    filter(TIME > "06:00:00" & TIME < "20:00:00") %>%
     select(-c(provider, event_type, event_type_reason, device_type))
+  print(df)
   return(df)
 }
 
@@ -15,31 +25,24 @@ postClean <- function(df){
   return(df)
 }
 
-splitTimeCol <- function(df){
-  "Splits the data column into seperate day and time"
-  split <- str_split_fixed(df$event_time, " ", 2)
-  df <- df %>% select(-c(event_time)) %>% 
-    add_column(DATE = split[,1], TIME = split[,2])
-  return(df)
-}
-
 
 file <- "~/Documents/syncthing/school/summerResearch/data/availDemand/events2019.csv"
 
-# pickups <- read_csv(file) %>%
-#   preClean() %>%
-#   mapToTract() %>%
-#   splitTimeCol() %>%
-#   postClean() %>%
-#   group_by(TRACT)
-pickupsSummary <- tibble(TRACT=numeric(), DATE=character(), TRIPS=numeric())
-tracts <- pickups %>% distinct(TRACT) %>% pull()
-for (i in 1:length(tracts)){
-  tract <- tracts[i]
+pickups <- read_csv(file) %>%
+  splitTimeCol() %>%
+  preClean() %>%
+  mapToTract() %>%
+  postClean() %>%
+  group_by(TRACT)
+pickupsSummary <- tibble(DATE=character(), TRIPS=numeric(), TRACT=numeric())
+tracts <- pickups %>% distinct(TRACT) %>% pull() # good
+for (i in 1:length(tracts)){ # good
+  tract <- tracts[i] # good
   tractPickups <- pickups %>% filter(TRACT == tract)
   dailyPickups <- tractPickups %>%
-    group_by(DATE) %>% summarize(DATE, TRIPS=n())
+    group_by(DATE) %>% summarize(DATE, TRIPS=n(), .groups="keep")
   dailyPickups$TRACT <- tract
+  print(dailyPickups)
   pickupsSummary <- pickupsSummary %>% bind_rows(dailyPickups)
 }
 
