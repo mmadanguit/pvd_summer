677086c2303de2467343e048ae98f222f168a58e
Marion <mmadanguit@olin.edu>
2020-07-08 08:03:40 -0500

    Separated function into two parts

--- scooterData/clusterSpectralEvaluate.R
+++ scooterData/clusterSpectralEvaluate.R
@@ -1,19 +1,24 @@
 # Calculate intra-cluster similarity using Euclidean distance ------------------
-calculateSim <- function(clusters, numClusters) {
+calculateSim <- function(clusters, i) {
+  points <- clusters %>% filter(sc == i)
+  points <- points[,!names(points) %in% "sc"]
+  # Create distance matrix between all points in cluster
+  # dist <- as.data.frame(distm(points, points, distGeo))
+  dist <- as.matrix(dist(points))
+  dist[dist == 0] <- NA
+  # For each point, find average distance to other points in the cluster
+  pointAvg <- rowMeans(dist, na.rm = TRUE)
+  # Find average distance over all points
+  clusterAvg <- mean(pointAvg)
+  return(clusterAvg)
+}
+
+avgSim <- function(clusters, numClusters) {
   # Initialize vector to store each cluster's average distance
   avgs <- c()
   # Loop through each cluster
   for (i in 1:numClusters) { 
-    points <- clusters %>% filter(sc == i)
-    points <- points[,!names(points) %in% "sc"]
-    # Create distance matrix between all points in cluster
-    # dist <- as.data.frame(distm(points, points, distGeo))
-    dist <- as.matrix(dist(points))
-    dist[dist == 0] <- NA
-    # For each point, find average distance to other points in the cluster
-    pointAvg <- rowMeans(dist, na.rm = TRUE)
-    # Find average distance over all points
-    clusterAvg <- mean(pointAvg)
+    clusterAvg <- calculateSim(clusters, i)
     avgs[i] <- clusterAvg
   }
   # Replace one-node cluster NA distances with 0 
