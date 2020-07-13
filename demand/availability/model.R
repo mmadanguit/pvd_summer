c4fe49c4b6c1182eda311ded865921ac952b4a04
mstites <maeve@mstites.com>
2020-07-13 13:42:29 -0400

    Start optimization for tiny app

--- demand/availability/model.R
+++ demand/availability/model.R
@@ -2,16 +2,10 @@ library(tidyverse)
 library(sf)
 library(mapview)
 setwd("~/Documents/github/pvd_summer/") # change to your WD
-fol <<- "~/Documents/syncthing/school/summerResearch/data/availDemand/"
-
-startD <<- "2019-12-01"
-endD <<- "2019-12-31"
 
 getAvail <- function(fol){
   "Open, filter, and clean availability data"
-  availIntervals <- read_csv(paste(c(fol, "availIntervals2019.csv"), collapse='')) %>%
-    # bind_rows(read_csv(paste(c(fol, "pickupsSummary2018.csv"), collapse=''))) %>%
-    filter(DATE >= startD & DATE <= endD) %>%
+  availIntervals <- read_csv(paste(c(fol, "availIntervals.csv"), collapse='')) %>%
     group_by(TRACT) %>% 
     arrange(DATE, .by_group=TRUE)
   availTime <- availIntervals %>% 
@@ -23,10 +17,7 @@ getAvail <- function(fol){
 }
 
 getPickups <- function(fol){
-  pickups <- read_csv(paste(c(fol, "pickupsSummary2018.csv"), collapse='')) %>%
-    bind_rows(read_csv(paste(c(fol, "pickupsSummary2019.csv"), collapse=''))) %>%
-    filter(DATE >= startD & DATE <= endD) %>%
-    select(-c(X1)) %>% # remove column of index markers
+  pickups <- read_csv(paste(c(fol, "pickupsSummary.csv"), collapse='')) %>%
     group_by(TRACT) %>% 
     arrange(DATE, .by_group=TRUE)
   return(pickups)
@@ -46,8 +37,9 @@ dailyAvg <- function(demand){
   "Find daily average for trip data over period of time.
   demand: demand dataframe"
   print(demand)
-  dAvg <- demand %>% 
-    group_by(TRACT) %>%
+  dAvg <- demand %>% group_by(TRACT)
+  if("AVAIL" %in% colnames(demand)){
+  dAvg <- dAvg %>%
     summarize(meanTrips = mean(ADJTRIPS, na.rm = TRUE),
               medTrips = median(ADJTRIPS, na.rm = TRUE),
               stdTrips = sd(ADJTRIPS, na.rm = TRUE),
@@ -58,21 +50,44 @@ dailyAvg <- function(demand){
               zeroAvail = sum(AVAIL == 0, na.rm = TRUE), # days w/ zero avail
               naAvail = sum(is.na(AVAIL))) %>%
     drop_na()
+  }
+  else {
+    dAvg <- dAvg %>%
+      summarize(meanTrips = mean(TRIPS, na.rm = TRUE),
+                medTrips = median(TRIPS, na.rm = TRUE),
+                stdTrips = sd(TRIPS, na.rm = TRUE),
+                zeroTrips = sum(TRIPS == 0, na.rm = TRUE)) %>%
+      drop_na()
+  }
   dAvg[dAvg == Inf] <- NA
   return(geomData(dAvg))
 }
 
-constDemand <- function(availTime, pickups){
-  availTime <- getAvail(fol)
-  pickups <- getPickups(fol) %>% 
-    filter(TRACT %in% availTime$TRACT) # filter to tracts which exist within data
-  pickups$ADJTRIPS <- pickups$TRIPS/(availTime$AVAIL/960)
+constData <- function(fol, avail = FALSE){
+  "Construct demand dataframe
+  fol: Folder path containing pickupsSummary.csv and availabilitySummary.csv
+  avail: Availability only model
+  "
+  pickups <- getPickups(fol)
+  if (!avail){ 
+    availTime <- getAvail(fol) %>%
+      filter(TRACT %in% pickups$TRACT) # only tracts in data
+    pickups <- pickups %>%  
+      filter(TRACT %in% availTime$TRACT)
+    pickups$ADJTRIPS <- pickups$TRIPS/(availTime$AVAIL/960) # adjust for avail
+  }
+  else {
+    pickups$ADJTRIPS <- pickups$TRIPS    
+  }
   demand <- availTime %>%
     add_column(ADJTRIPS = pickups$ADJTRIPS)
   return(demand)
 }
 
-demand <- constDemand(availTime, pickups)
+# assumes working directory in pvd_summer to grab geo data
+fol <- "~/Documents/syncthing/school/summerResearch/data/availDemand/"
+# data available on drive: Data/demand
+demand <- constData(fol)
 demand <- demand %>%
   # filter() %>%
   dailyAvg()
