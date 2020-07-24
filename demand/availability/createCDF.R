68b19f9aabebc2aeb935fd2ae6d5074b56649fe0
Marion <mmadanguit@olin.edu>
2020-07-23 23:46:08 -0500

    Convert time to minutes

--- demand/availability/createCDF.R
+++ demand/availability/createCDF.R
@@ -2,14 +2,11 @@ library(ggplot2)
 library(lubridate)
 library(tidyverse)
 
-# Import trip data ---------------------------------------------------
-# dir <- "~/Documents/syncthing/school/summerResearch/data/availDemand"
-# dir <- "" #Folder containing pickupsSummary.csv, availIntervals.csv, and tripsPerTract.csv
-fol <- ""
-filename <- "tripsYear1WithTracts.csv"
-# path <- file.path(dir, paste(filename, ".csv", sep = ""))
-# assign(tripsYear1WithTracts, read.csv(filename))
-tripsYear1WithTracts <- read.csv(filename)
+# Import and clean trip data ---------------------------------------------------
+dir <- "/home/marion/PVDResearch/Data/mobilityData/cleanData"
+filename <- "tripsYear1WithTracts"
+path <- file.path(dir, paste0(filename, ".csv"))
+assign(filename, read.csv(path))
 
 tripData <- tripsYear1WithTracts %>%
     # Consider only trips that last at least 3 minutes
@@ -20,50 +17,25 @@ tripData <- tripsYear1WithTracts %>%
     # Separate start time into date and time
     mutate(date = as.Date(startTime),
            startTime = format(startTime, format = "%H:%M:%S")) %>%
-    # Convert time to seconds
-    mutate(startSec = period_to_seconds(hms(startTime))) %>%
+    # Convert time to minutes
+    mutate(startMin = period_to_seconds(hms(startTime))/60) %>%
     # Consider only trips that began within operating hours
-    filter(startSec >= 6*60*60 & startSec < 22*60*60) %>%
+    filter(startMin >= 6*60 & startMin < 22*60) %>%
     # Select only relevant columns
-    select(date, startTime, startSec)
+    select(date, startMin)
 
 # Plot hourly average usage ----------------------------------------------------
 usageData <- tripData %>%
-  mutate(startSec = round(startSec, digits = -4)) %>%
-  group_by(date, startSec) %>%
+  mutate(startMin = round(startMin, digits = -2)) %>%
+  group_by(date, startMin) %>%
   summarise(count = n()) %>%
-  group_by(startSec) %>%
+  group_by(startMin) %>%
   summarise(count = mean(count))
 
-usagePlot <- ggplot(usageData, aes(x = startSec, y = count, group = 1)) +
+ggplot(usageData, aes(x = startMin, y = count, group = 1)) +
   geom_line() + 
   labs(title = "Hourly Average Scooter Usage")
 
 # Create CDF -------------------------------------------------------------------
-cdf <- ecdf(tripData$startSec)
+cdf <- ecdf(tripData$startMin)
 plot(cdf)
-
-# smoothFunc <- function(adj = 0.1) {
-#   "Smooth cdf by generating a kernel density estimate"
-#   d = tripData$startSec
-#   # Generate kernel density estimate of data
-#   dens = density(d, adjust = adj, from = min(d), to = max(d))
-#   dens = data.frame(x = dens$x, y = dens$y)
-#   # Plot kernel density
-#   ggplot(as.data.frame(d), aes(d)) + 
-#     geom_line(data = dens, aes(x = x, y = cumsum(y)/sum(y))) +
-#     labs(title = paste("adj =", adj))
-#   return(data.frame(x = dens$x, y = cumsum(dens$y)/sum(dens$y)))
-# }
-# 
-# cdfSmooth <- function(times){
-#   "Calculates cd estimates based on smoothed cdf
-#   times: vector storing trip times in seconds"
-#   d <- smoothFunc()
-#   estimates <- c()
-#   for (i in 1:length(times)) {
-#     ind <- which.min(abs(d$x-times[i])) # Find closest value
-#     estimates[i] <- d$y[ind]
-#   }
-#   return(estimates)
-# }
