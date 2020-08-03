ba93cd362934ac9b1f8cea3937b5a40d4fcfeff2
mstites <maeve@mstites.com>
2020-08-03 16:25:06 -0400

    Change rounding value loc

--- app.R
+++ app.R
@@ -83,7 +83,7 @@ makeCensusPlot <- function(item, varToPlot){
       smoothFactor = 0,
       fillOpacity = 0.7,
       fillColor = ~ pal(get(item)),
-      label = ~round(get(item), 1),
+      label = ~round(get(item), 3),
       labelOptions = labelOptions(noHide = TRUE, direction = 'center', textOnly = TRUE),
       popup = popupHTML, #Add the label
       highlightOptions = highlightOptions(color = "#FFFFFF", weight = 2, bringToFront = TRUE)
