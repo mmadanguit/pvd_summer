c7e2181b526424f8cadb10231c4f333e00f16ca8
hkRho <hrho@olin.edu>
2020-08-03 04:07:51 -0400

    Propensity scores and matching

--- college/linear_regression.R
+++ college/linear_regression.R
@@ -4,6 +4,7 @@ library(pracma)
 library(ggplot2)
 library(lubridate)
 library(tigris)
+library(MatchIt)
 source('downtown_loc.R')
 source('college_loc.R')
 source('mapToTract.R')
@@ -47,7 +48,7 @@ dist_to_downtown <- function(data) {
   
   # check whether their distance is within the college_locs
   for (i in 1:dim(loc)[1]) {
-    min_dist <- 987654321
+    min_dist <- 2000
     for (j in 1:dim(downtown_loc)[1]) {
       dist <- haversine(c(loc$LAT[i], loc$LNG[i]), c(downtown_loc$LAT[j], downtown_loc$LNG[j]), MI_RADIUS)
       if (dist < min_dist) { # if dist is smaller than min dist 
@@ -68,7 +69,7 @@ dist_to_college <- function(data) {
   
   # check whether their distance is within the college_locs
   for (i in 1:dim(loc)[1]) {
-    min_dist <- 987654321
+    min_dist <- 2000
     for (j in 1:dim(college_loc)[1]) {
       dist <- haversine(c(loc$LAT[i], loc$LNG[i]), c(college_loc$LAT[j], college_loc$LNG[j]), MI_RADIUS)
       if (dist < min_dist) { # if dist is smaller than min dist 
@@ -76,6 +77,25 @@ dist_to_college <- function(data) {
       }
     }
     data[(data$LAT == loc$LAT[i] | data$LNG == loc$LNG[i]),]$COLLDIST <- min_dist
+  }
+  
+  return(data)
+}
+
+
+near_college_check <- function(data, buffer) {
+  MI_RADIUS <- 3956
+  college_loc <- get_colleges()
+  loc <- get_unique_loc(data)
+  
+  # check whether their distance is within the college_locs
+  for (i in 1:dim(loc)[1]) {
+    for (j in 1:dim(college_loc)[1]) {
+      dist <- haversine(c(loc$LAT[i], loc$LNG[i]), c(college_loc$LAT[j], college_loc$LNG[j]), MI_RADIUS)
+      if (dist <= buffer) {
+        data[(data$LAT == loc$LAT[i] | data$LNG == loc$LNG[i]),]$NEARCOLLEGE <- TRUE
+      }
+    }
   }
   
   return(data)
@@ -161,10 +181,11 @@ get_density <- function(data) {
 # -------------- PREP PICKUP DATA --------------------
 pickups <- read_csv("~/PVD Summer Research/average_num_available/intervalCountsLATLNG.csv")
 pickups <- pickups %>%
-  filter(DATE >= "2019-4-15" & DATE <= "2019-5-15") %>%
-  mutate(DOWNDIST = 0, COLLDIST = 0) %>%
+  filter(DATE >= "2019-4-15" & DATE <= "2019-6-15") %>%
+  mutate(DOWNDIST = 0, COLLDIST = 0, NEARCOLLEGE = FALSE) %>%
   dist_to_downtown() %>%
-  dist_to_college()
+  dist_to_college() %>%
+  near_college_check(0.2)
 
 
 # -------------- GEOID RETRIEVAL ---------------------
@@ -177,11 +198,6 @@ ri_wac <- read_csv("~/PVD Summer Research/college/ri_wac_S000_JT00_2017.csv") %>
   mutate(ROUNDED_GEOID = str_sub(as.character(w_geocode), 1, 11))
 
 ri_dat <- read_csv("~/PVD Summer Research/pvd_summer/censusData/riData.csv")
-
-# include median income, mean commute (1)
-# plot residuals versus each variable (2)
-# include everything but the levels
-# look at teh correlation sbetween teh variables
 
 data <- data %>%
   mutate(ROUNDED_GEOID = str_sub(as.character(GEOID), 1, 11)) %>%
@@ -202,18 +218,76 @@ na_data <- data[!complete.cases(data), ]
 data <- data %>%
         na.omit()
 
+
 # ------- FIND CORRELATION BETWEEN VAIRABLES ---------
 # pairs(data[, c(3, 9, 10, 14:21)], lower.panel=panel.smooth, upper.panel=panel.cor)
 
 
 # -------------- LINEAR REGRESSION MODEL -------------
-# log0 = error, so added 1 to the count when taking the log
-model <- lm(COUNT ~ COLLDIST + DOWNDIST + TOTJOBS + POP + PERCAPITAINC + 
-                    AUTO + PUBLIC + WALK + COLLEGE + POVERTY, data = data)
-print(summary(model))
-print(summary(model)$coefficient)
-print(summary(model)$r.squared)
-print(sigma(model)/mean(data$COUNT))
+## DO TRANSFORMATIONS ON HIGH VARIANCE
+## FIND HIGHEST INDICES FROM RESIDUAL AND UNDERSTAND WHERE THEY ARE COMING FROM
 
-layout(matrix(c(1,2,3,4),2,2)) # optional 4 graphs/page
-plot(model)
+# log0 = error, so added 1 to the count when taking the log
+# model <- lm(log(COUNT+1) ~ COLLDIST + log(DOWNDIST+1) + TOTJOBS + log(POP+1) + log(PERCAPITAINC+1) +
+#                     AUTO + PUBLIC + WALK + COLLEGE + POVERTY, data = data)
+# print(summary(model))
+# print(summary(model)$coefficient)
+# print(summary(model)$r.squared)
+# print(sigma(model)/mean(data$COUNT))
+# 
+# ## plot each variable against residuals to see the variable datapoints' distribution
+# plot(data$POVERTY,model$residuals)
+# 
+# ## find the outliers with the highest squared residuals
+# o <- order(model$residuals^2, decreasing=T)
+# print(data[o[1:20],c(5:21)])
+# 
+# 
+# layout(matrix(c(1,2,3,4),2,2)) # optional 4 graphs/page
+# plot(model)
+# 
+# 
+# ----------- MATCHING & PROPENSITY SCORES -----------
+## add new column of close or far from college value
+matching_data <- data %>%
+                 mutate(NEARCOLLEGE = ifelse(NEARCOLLEGE, 1, 0)) %>%
+                 ungroup()
+matching_data <- as.data.frame(matching_data)
+str(matching_data)
+
+
+# ------------------- PREPROCESSING ------------------
+## 1. Standardized Difference
+treated <- (matching_data$NEARCOLLEGE==1)
+cov <- matching_data[, c(15, 9, 16, 18, 22)]
+std.diff <- apply(cov, 2, function(x) 100*(mean(x[treated]) - mean(x[!treated])) / (sqrt(0.5*(var(x[treated]) + var(x[!treated])))))
+abs(std.diff)
+
+## 2. Chi-square Test
+library("RItools")
+
+xBalance(NEARCOLLEGE ~ TOTJOBS + DOWNDIST + POP + PERCAPITAINC + POVERTY, data = matching_data,
+         report = c("chisquare.test"))
+
+
+# ----------- PROPENSITY SCORE ESTIMATION -------------
+ps <- glm(NEARCOLLEGE ~ TOTJOBS + DOWNDIST + POP + PERCAPITAINC + POVERTY, data = matching_data,
+          family = binomial())
+summary(ps)
+
+
+matching_data$psvalue <- predict(ps, type = "response")
+library("Hmisc")
+histbackback(split(matching_data$psvalue, matching_data$NEARCOLLEGE), main = "Propensity Score Before Matching", 
+             xlab=c("control", "treatment"))
+
+
+## conventional matching using Mahalanobis distance - DOESNT WORK
+m.mahal <- matchit(NEARCOLLEGE ~ TOTJOBS + DOWNDIST + POP + PERCAPITAINC + POVERTY, data = matching_data,
+                   mahvars = c("TOTJOBS", "DOWNDIST", "POP", "PERCAPITAINC", "POVERTY"),
+                   caliper = 0.25, calclosest = TRUE, replace = TRUE, distance = "mahalanobis")
+summary(m.mahal)
+
+m.nn <- matchit(NEARCOLLEGE ~ TOTJOBS + DOWNDIST + POP + PERCAPITAINC + POVERTY, data = matching_data,
+                method = "nearest", ratio = 2)
+summary(m.nn)
